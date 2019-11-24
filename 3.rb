20b3e099c4060be5ce4198f42e99e92907520ab2
Ivan <ak_dlia_hc_ru@mail.ru>
2019-11-24 23:48:19 +0300

    Lesson 2

--- 3.rb
+++ 3.rb
@@ -6,8 +6,6 @@ fibonachi = [0, 1]
 loop do
   next_element = fibonachi[-2] + fibonachi[-1]
   break if next_element > 100
-
-  fibonachi << next_element
+  fibonachi.push(next_element)
 end
-
 print fibonachi
